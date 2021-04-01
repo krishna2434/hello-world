1f9ca69877c1541dca0d3a80490db3b24ea5bc42
nani1 <nani1@mail.com>
2021-04-01 16:33:11 +0530

    My First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello world")
+
+#!/usr/bin/env python
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('I update automatically!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
